17a325d83d85bb3ff8e04f686caba533b14d0c61
kjfg 王 <kjfg_mcbbs@qq.com>
2024-02-06 23:20:06 +0800

    清理

--- lib/config.py
+++ lib/config.py
@@ -1,17 +1,17 @@
 
 # 由于录播姬的webhook输出的是相对路径，需要补充工作路径
-rechimedir = "/home/luopu/record/rechime"
+rechimedir = "/home/patch/record/rechime"
 
-# 由于blrec的webhook是绝对路径，并且没有用户名相关信息，需要补充存放目录
+# 由于blrec的webhook是绝对路径，需要补充存放目录让其上传路径正常识别
 
-blrecdir = "/home/luopu/record/blrec"
+blrecdir = "/home/patch/record/blrec"
 
 # —————————————rclone设置——————————————
-# rclone上传目录 务必加入最后的"/"
-rclonedir = "od:测试文件夹/"
-# rclone的上传模式 默认为move(移动)
+# rclone上传目录 
+rclonedir = "remote:path"
+# rclone的上传模式 默认为move(剪切),默认情况下上传失败不会删除源文件
 rcmode = "move"
-# 请在下面填写rclone的配置文件
-rcconf = "C:/Users/kjfg_/AppData/Roaming/rclone/rclone.conf"
-# 以下填写rclone主程序路径
-rclone = "D:/rclone-v1.65.2-windows-amd64/rclone.exe"
+# 请在下面填写rclone的配置文件路径
+rcconf = "/rclone/rclone.conf"
+# 以下填写rclone主程序路径/shell命令
+rclone = "rclone.exe"
