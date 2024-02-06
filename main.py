1f4ddfb4fd0492ccc1b18aec72feaedf4cd3b7eb
kjfg 王 <kjfg_mcbbs@qq.com>
2024-02-06 23:10:48 +0800

    Change webhook subpath

--- main.py
+++ main.py
@@ -10,6 +10,6 @@ logging.basicConfig(level=logging.DEBUG)
 
 if __name__ == "__main__":
     app = web.Application()
-    app.add_routes([web.post("/webhook", rechime.handle_webhook)])
+    app.add_routes([web.post("/rechime", rechime.handle_webhook)])
     app.add_routes([web.post("/blrec", blrec.handle_webhook)])
     web.run_app(app, host="0.0.0.0", port=10801)
